212966ed7d955f89306af057bb1842b4f3986213
Robert Eanes <github@robsinbox.com>
2016-03-01 10:26:05 -0600

    handle malformed parameters more gracefully

--- lib/restpack_serializer/serializable/paging.rb
+++ lib/restpack_serializer/serializable/paging.rb
@@ -67,7 +67,7 @@ module RestPack::Serializer::Paging
       return url unless params.any?
       normalized_params = []
       params.each do |param_string|
-        normalized_params += param_string.split("&")
+        normalized_params += param_string.split(/[&?]/)
       end
       hash_params = Hash[normalized_params.map { |p| p.split("=") }.sort]
       url += '?' + hash_params.to_query if params.any?
